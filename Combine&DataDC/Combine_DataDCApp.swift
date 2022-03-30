ad338d486fd0b3dbaa8924d520d55eed4328dc4f
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-03-30 16:34:51 -0600

    Initial Commit

---
+++ Combine&DataDC/Combine_DataDCApp.swift
@@ -0,0 +1,17 @@
+//
+//  Combine_DataDCApp.swift
+//  Combine&DataDC
+//
+//  Created by Mitch Andrade on 3/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Combine_DataDCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
