cbdf0893d23b5baf94cdc8f271f8b678f0dd39c6
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-03-31 23:32:23 -0600

    third commit

---
+++ Combine&DataDC/Components/Buttons/PrimaryButton.swift
@@ -0,0 +1,20 @@
+//
+//  PrimaryButton.swift
+//  Combine&DataDC
+//
+//  Created by Mitch Andrade on 3/31/22.
+//
+
+import SwiftUI
+
+struct PrimaryButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PrimaryButton_Previews: PreviewProvider {
+    static var previews: some View {
+        PrimaryButton()
+    }
+}
---
+++ Combine&DataDC/Utils/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  Combine&DataDC
+//
+//  Created by Mitch Andrade on 3/31/22.
+//
+
+import Foundation
