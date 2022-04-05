8bedd17f83c22dc2c7c5b6f9aa9d2a0f85443122
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-04-05 10:38:15 -0600

    fifth commit

---
+++ Combine&DataDC/Components/Rows/LiteModeRow.swift
@@ -0,0 +1,20 @@
+//
+//  LiteModeRow.swift
+//  Combine&DataDC
+//
+//  Created by Mitch Andrade on 4/3/22.
+//
+
+import SwiftUI
+
+struct LiteModeRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LiteModeRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LiteModeRow()
+    }
+}
